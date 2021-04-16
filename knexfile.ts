148b532dced8e150e0cc27dedfa751b1014f0648
luis <luis.sg.vitor@gmail.com>
2021-04-16 16:13:56 -0300

    refactor: creating trigger to updated_at in database

--- knexfile.ts
+++ knexfile.ts
@@ -52,6 +52,13 @@ export default {
     migrations: {
       tableName: "knex_migrations"
     }
-  }
+  },
+
+  onUpdateTrigger: (table:string) => `
+    CREATE TRIGGER ${table}_updated_at
+    BEFORE UPDATE ON ${table}
+    FOR EACH ROW
+    EXECUTE PROCEDURE on_update_timestamp();
+  `
 
 };
--- src/database/migrations/20210413013621_create_posts.ts
+++ src/database/migrations/20210413013621_create_posts.ts
@@ -1,5 +1,8 @@
 import { Knex } from "knex";
 
+import configs from '../../../knexfile'
+const { onUpdateTrigger } = configs
+
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('posts', table => {
     table.uuid('id').notNullable().unique().defaultTo(knex.raw('uuid_generate_v4()')).primary()
@@ -9,7 +12,7 @@ export async function up(knex: Knex): Promise<void> {
 
     table.timestamp('created_at').notNullable().defaultTo(knex.fn.now())
     table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now())
-  })
+  }).then(() => knex.raw(onUpdateTrigger('posts')))
 }
 
 
