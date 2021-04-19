539ac8b868822fc13ee209cb5b9c516c0a33904b
luis <luis.sg.vitor@gmail.com>
2021-04-19 18:16:13 -0300

    chore: adding redis

---
+++ src/services/redis.ts
@@ -0,0 +1,13 @@
+import Redis from 'ioredis'
+
+const redis = new Redis()
+
+const cache = {
+  set: (key: string, value: string, seconds: string) => (
+    redis.set(key, value, 'EX', seconds)
+  ),
+  get: (key:string) => redis.get(key),
+  del: (key:string) => redis.del(key)
+}
+
+export default cache
